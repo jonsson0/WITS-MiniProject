47298cfc159b5e5c7f057f8bdfe7c4071d07b54a
Mads Jönsson <madsjonsson1997@gmail.com>
2020-11-12 01:55:31 +0100

    A shitton of new shit, it works atm

--- MainSite.php
+++ MainSite.php
@@ -1,14 +1,62 @@
 <?php
+session_start();
+
 require_once 'TopNavBar.html';
+require_once '/home/mir/forum/forum.php';
+$loggedIn = $_SESSION['loggedIn'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>MainSite</title>
 </head>
 <body>
+<?php
+if ($loggedIn == true) {
+    echo $_SESSION['loggedIn'];
+    echo "<br>";
+    echo "we are logged in baby";
 
+} else {
+    echo "Go to the login page to login";
+}
+?>
+<br>
+<div class="TableOfPosts">
+    <table>
+        <?php
+        // get all the posts and save in a var
+        $arrayOfPosts = get_posts();
+        // get the number of elements in the array (number of posts and replys in total)
+        $numberOfPosts = count($arrayOfPosts);
+
+        // running through all the posts
+        for ($i = 1; $i < $numberOfPosts; $i++) {
+            // save number i post in a var
+            $post = get_post_by_pid($i);
+
+
+            // if its a Main post (!= reply)
+            if ($post['parent_pid'] == 0) {
+                // saved the main posts pid, title, content, uid
+                // and echoes out a new tr with td which is the text of the main post
+                $postPID = $post['pid'];
+                $postTitle = $post['title'];
+                $postContent = $post['content'];
+                $posterUID = $post['uid'];
+                // getting the name of the poster by using the UID
+                $user = get_user_by_uid($posterUID);
+                $nameOfUser = $user['name'];
+                $dateOfPost = $post['date'];
+
+                echo "<tr class='Post'> <td class='PostTitle'>$postTitle</td> <td class='PostContent'>This is post number $postPID: $postContent</td> <td class='Poster'>Posted by: $nameOfUser</td> <td class='Date'>Posted on: $dateOfPost</td> </tr>";
+            }
+        }
+        ?>
+
+    </table>
+</div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -2,12 +2,15 @@
 session_start();
 require_once '/home/mir/forum/forum.php';
 require_once 'TopNavBar.html';
+
 $uid= $_GET['uid'];
 $password= $_GET['password'];
 
 if (isset($_GET['submit'])) {
   if (login($uid, $password)) {
     echo "du er nu logget på";
+      $_SESSION['loggedIn'] = true;
+      header("Location: MainSite.php");
   } else {
     echo "login fejlede";
   }
@@ -22,12 +25,17 @@ if (isset($_GET['submit'])) {
   </head>
   <body>
     <h1>Login</h1>
-    <form action="login.php" method="get">
-     <table>
-     <tr><td>Username:</td> <td><input type="text" name="uid" required/></td></tr>
-     <tr><td>Password:</td> <td><input type="password" name="password" required/></td></tr>
-     <tr><td></td> <td><input type="submit" name="submit" value="Login"></td></tr>
-     </table>
+    <form action="login.php" method="GET">
+        <table>
+            <tr><td>Username:</td> <td><input type="text" name="uid" required/></td></tr>
+            <tr><td>Password:</td> <td><input type="password" name="password" required/></td></tr>
+            <tr><td></td> <td><input type="submit" name="submit" value="Login"></td></tr>
+        </table>
     </form>
+
+    <br>
+  <div>
+      You dont have an account yet? <a href="signup.php"> Click here to make an account</a>
+  </div>
   </body>
 </html>
